1773d09b63abb3151d272451d34c19d09422f48f
heycn <x1chennan@qq.com>
2022-01-25 20:14:33 +0800

    历史页js

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,21 @@ Page({
     curLang: {} // 选择语言
   },
 
+  onLoad: function (options) {
+    console.log('lonload..')
+    console.log(options)
+    if (options.query) {
+      this.setData({ query: options.query })
+    }
+  },
+
+  onShow: function () {
+    if (this.data.curLang.lang !== app.globalData.curLang.lang) {
+      this.setData({ curLang: app.globalData.curLang })
+      this.onConfirm()
+    }
+  },
+
   onInput: function (e) {
     this.setData({ query: e.detail.value })
     if (this.data.query.length > 0) {
@@ -24,5 +39,19 @@ Page({
 
   onConfirm: function () {
     if (!this.data.query) return
+    translate(this.data.query, {
+      from: 'auto',
+      to: this.data.curLang.lang
+    }).then(res => {
+      this.setData({ result: res.trans_result })
+
+      let history = wx.getStorageSync('history') || []
+      history.unshift({
+        query: this.data.query,
+        result: res.trans_result[0].dst
+      })
+      history.length = history.length > 10 ? 10 : history.length
+      wx.setStorageSync('history', history)
+    })
   }
 })
